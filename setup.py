bf2e326d7f262c01bac73db32f301e9fe3963b6b
kiyou <46545721+kiyou@users.noreply.github.com>
2021-06-21 07:17:22 +0000

    [mod] update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 import setuptools
 
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
 setuptools.setup(
     name="interfacemaster",
     version="1.1.1",
-    install_requires=[
-        "pymatgen",
-    ],
+    install_requires=_requires_from_file('requirements.txt'),
     author="Yaoshu Xie",
     author_email="yxie@iis.u-tokyo.ac.jp",
     description="A program of calculating interface structure",
