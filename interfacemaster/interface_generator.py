3cc371021817bced9d4e88aef5ce4ae5b8dc222c
Hongyi Zhao <hongyi.zhao@gmail.com>
2024-03-07 22:00:45 +0800

    import dot from numpy.

--- interfacemaster/interface_generator.py
+++ interfacemaster/interface_generator.py
@@ -4,7 +4,7 @@ interface_generator.py
 """
 import os
 from numpy.linalg import det, norm, inv
-from numpy import (cross, cos, sin, array, column_stack,
+from numpy import (cross, cos, dot, sin, array, column_stack,
                    eye, arccos, around, sqrt)
 from pymatgen.core.lattice import Lattice
 from pymatgen.core.structure import Structure
